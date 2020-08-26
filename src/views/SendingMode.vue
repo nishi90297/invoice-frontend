2677768325b7c2b93bb4cfbf34397b8bedb340c3
Nishtha Garg <nishi.90297@gmail.com>
2020-08-27 00:55:26 +0530

    remaining mail body

--- src/views/SendingMode.vue
+++ src/views/SendingMode.vue
@@ -18,24 +18,62 @@
     <div class="columns" v-if="preferences.mail.useThis">
       <div class="column is-6">
         <div class="box">
-
+          <div class="column">
+            <p class="has-text-weight-semibold">The payer will receive the mail shown in image from -
+              <span class="has-text-primary">no-reply@invoicer.com</span>
+            </p>
+            <b-field label="Payer Email" class="mt3">
+              {{ preferences.mail.emailId }}
+            </b-field>
+            <b-field label="Mail Subject" class="mt3">
+              <b-input maxlength="100" type="text" v-model="preferences.mail.subject" />
+            </b-field>
+            <b-field label="Mail Message" class="mt3">
+              <b-input maxlength="250" type="textarea" rows="5" v-model="preferences.mail.message" />
+            </b-field>
+          </div>
         </div>
       </div>
       <div class="column is-6">
         <div class="columns">
           <div class="column is-12">
-            <div class="box">
-              dewf
+            <div class="box ">
+              <span class="has-text-weight-bold">Subject</span> : {{preferences.mail.subject}}
             </div>
           </div>
         </div>
         <div class="columns">
           <div class="column is-12">
             <div class="box">
-              dewf
+              <div class="columns">
+                ICON
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <p class="has-text-weight-bold">Dear {{userInfo.name.first+" "+ userInfo.name.last}},</p>
+                </div>
+                <div class="column">
+                  <p class="mt1">{{preferences.mail.message}}</p>
+                </div>
+                <hr class="has-background-grey-lighter">
+              </div>
+              <div class="columns">
+                <div class="column">
+                  <p class="has-text-weight-bold">Rs {{invoice.total}}.00</p>
+                </div>
+                <div class="column">
+                  <p class="mt1">Due Date- {{invoice.dueDate}}</p>
+                </div>
+                <hr class="has-background-grey-lighter">
+              </div>
             </div>
           </div>
         </div>
+      </div>
+    </div>
+    <div class="columns" style="position: fixed; bottom: 2%; right: 1%;width: 83.3%;" >
+      <div class="column has-text-right has-background-primary ">
+        <b-button @click="sendModeContinue()">Continue</b-button>
       </div>
     </div>
   </section>
@@ -51,6 +89,10 @@
       return{
         invoice:{},
         userInfo:{
+          name:{
+            first:'',
+            last:''
+          },
           email:'',
         },
         preferences:{
@@ -97,6 +139,9 @@
           this.info('Backend Error');
           console.log(error);
         })
+      },
+      sendModeContinue(){
+
       }
     },
     mounted() {
@@ -107,5 +152,8 @@
 </script>
 
 <style scoped>
+
+
+
 
 </style>
